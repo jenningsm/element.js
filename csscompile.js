6d2caf9bbb6f415646cce860c6422cf75c8bb48b
Michael Jennings <mpjngs@gmail.com>
2015-03-24 16:26:34 -0700

    working on csscompile

---
+++ csscompile.js
@@ -0,0 +1,63 @@
+
+
+
+function compileCSS(styles){
+
+  //create a set of all styles. this gets rid of duplicates
+  var styleSet = {};
+  for(var i = 0; i < styles.length; i++){
+    for (var j = 0; j < styles[i].length; j++){
+      styleSet[styles[i][j]] = true;
+    }
+  }
+
+  //turn the set into an ordered list
+  var styleList = styleSet.keys();
+
+  //index the styles of each element
+  var hashes = [];
+  for(var i = 0; i < styles.length; i++){
+    var hash = {};
+    for(var j = 0; j < styles[i].length; j++){
+      hash[styles[i][j]] = true;
+    }
+    hashes.push(hash);
+  }
+
+   /*
+     For every pair of styles, determine if there is at least one element in which one of styles occurs and the other doesn't.
+     If there is no such element, match those styles. create a set of partitions of collectively matched styles
+   */
+
+   var partitions = [];
+
+   var matched = [];
+   for(var i = 0; i < styleList.length; i++){
+     matched.push(false);
+   }
+
+   for(var i = 0; i < styleList.length; i++){
+     if(!matched[i]){
+       var partition = [i];
+       for(var j = i + 1; j < styleList.length; j++){
+         var match = true;
+         for(var k = 0; k < hashes.length; k++){
+           var first = (styleList[i] in hashes[k]);
+           var second = (styleList[j] in hashes[k]);
+           if(first !== second){
+             match = false;
+             break;
+           }
+         }
+         if(match){
+           partition.push(j);
+           matched[j] = true;
+         }
+       }
+       partitions.push(partition);
+     }
+   }
+
+   
+
+}
