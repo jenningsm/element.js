09bb535ede0071c5ef7cbfb54e2f062955e1dbad
Michael Jennings <mpjngs@gmail.com>
2015-03-24 22:56:38 -0700

    more html stuff

--- html.js
+++ html.js
@@ -1,17 +1,58 @@
 
+function checkAttributes(attributes, value){
+  if(Array.isArray(attributes[0])){
+    for(var i = 0; i < attributes.length; i++){
+      if(attributes[i][0] === 'class'){
+        console.error("ERROR: You may not set the class property of an element");
+        return -1;
+      }
+      if(attributes[i][0] === 'style'){
+        console.warn("WARNING: Are you sure you want to set the style attribute?");
+        return 1;
+      }
+    }
+  } else {
+    
+    if(!Array.isArray(attribute)){
+      attributes = [attributes, value];
+    }
+
+    if(attributes[0] === 'class'){
+      console.error("ERROR: You may not set the class property of an element");
+      return -1;
+    }
+    if(attributes[0] === 'style'){
+      console.warn("WARNING: Are you sure you want to set the style attribute?");
+      return 1;
+    }
+  }
+  return 0;
+}
+
 /*
   tag: the tag of the element
-  attributes: an array of attribute-value pairs, in the form an array
+  attributes: an array of attribute-value pairs, in the form an array, or just a single attribute-value pair
 */
 function element(tag, attributes){
   var el = {};
   el.tag = tag;
+  el.styles = [];
+  el.content = [];
+
+  if(checkAttributes(attributes) === -1){
+     return;
+  }
+
   if(attributes !== undefined){
-    el.attributes = attributes;
+    if(Array.isArray(attributes[0]){
+      el.attributes = attributes;
+    } else {
+      el.attributes = [attributes];
+    }
   } else {
     el.attributes = [];
   }
-  el.styles = [];
+
   return el;
 }
 
@@ -47,6 +88,11 @@ function style(element, style, value){
 
 // used the same way as style, except for attributes instead of styles
 function attribute(element, attribute, value){
+
+  if(checkAttributes(attribute, value) === -1){
+    return;
+  }
+
   if(value === undefined){
     if(Array.isArray(style[0])){
        element.attributes = element.styles.concat(attibute);
@@ -56,5 +102,22 @@ function attribute(element, attribute, value){
   } else {
     element.attributes = element.styles.concat([attribute, value]);
   }
+  return element;
+}
+
+//append content to the end of element's content
+function appendContent(element, content){
+  element.content.push(content);
+  return element;
+}
+
+//insert content. if no position is specified, insert at beginning, else
+//insert at position specified, or end if longer than length
+function insertContent(element, content, position){
+  if(position === undefined){
+    position = 0;
+  }
+  position = Math.min(position, element.content.length);
+  element.content.splice(position, 0, content);
   return element;
 }
