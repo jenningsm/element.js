381f27e2e5a39c0ddf699536d699cf68ffad4cd9
Michael Jennings <mpjngs@gmail.com>
2015-03-28 22:22:53 -0700

    reorganization plus css bug fix

---
+++ cssify.js
@@ -0,0 +1,73 @@
+
+var cc = require('./csscompile');
+
+function createStyleSheet(classes){
+  var stylesheet = '';
+  for(cls in classes){
+    if(classes.hasOwnProperty(cls)){
+      stylesheet += '.' + cls + '{\n';
+      for(var j = 0; j < classes[cls].length; j++){
+        stylesheet += "  " + classes[cls][j] + ';\n';
+      }
+      stylesheet += '}\n';
+    }
+  }
+  return stylesheet;
+} 
+
+function format(string){
+   string = string.replace(/^\s*/g, '');
+   string = string.replace(/\s*$/g, '');
+   string = string.replace(/\s+/g, ' ');
+   return string;
+}
+
+function iterator(root){
+  var currgen = [root];
+  var nextgen = [];
+  return function(){
+    if(currgen.length === 0){
+      if(nextgen.length === 0){
+        return null;
+      } else {
+        currgen = nextgen;
+        nextgen = [];
+      }
+    }
+    for(var i = 0; i < currgen[0].content.length; i++){
+      if(typeof currgen[0].content[i] !== 'string'){
+        nextgen.push(currgen[0].content[i]);
+      }
+    }
+    return currgen.splice(0, 1)[0];
+  }
+}
+
+function cssify(root){
+  var elements = [];
+  var iter = iterator(root);
+  var el;
+  while((el = iter()) !== null){
+    var keys = Object.keys(el.styles);
+    var styles = [];
+    for(var i = 0 ; i < keys.length; i++){
+      styles.push(format(keys[i]) + ":" + format(el.styles[keys[i]]));
+    }
+    elements.push(styles);
+  }
+
+  var a = cc.compile(elements);
+
+  var classes = a['elements'];
+  var iter = iterator(root);
+  for(var i = 0; i < classes.length; i++){
+    var el = iter();
+    if(classes[i].length !== 0){
+      el.classes = classes[i].join(' ');
+    }
+  }
+
+  return createStyleSheet(a['styles']); 
+}
+
+module.exports.cssify = cssify;
--- html.js
+++ html.js
@@ -1,74 +1,5 @@
 
-var cc = require('./csscompile');
-
-function createStyleSheet(classes){
-  var stylesheet = '';
-  for(cls in classes){
-    if(classes.hasOwnProperty(cls)){
-      stylesheet += '.' + cls + '{\n';
-      for(var j = 0; j < classes[cls].length; j++){
-        stylesheet += "  " + classes[cls][j] + ';\n';
-      }
-      stylesheet += '}\n';
-    }
-  }
-  return stylesheet;
-} 
-
-function format(string){
-   string = string.replace(/^\s*/g, '');
-   string = string.replace(/\s*$/g, '');
-   string = string.replace(/\s+/g, ' ');
-   return string;
-}
-
-function iterator(root){
-  var currgen = [root];
-  var nextgen = [];
-  return function(){
-    if(currgen.length === 0){
-      if(nextgen.length === 0){
-        return null;
-      } else {
-        currgen = nextgen;
-        nextgen = [];
-      }
-    }
-    for(var i = 0; i < currgen[0].content.length; i++){
-      if(typeof currgen[0].content[i] !== 'string'){
-        nextgen.push(currgen[0].content[i]);
-      }
-    }
-    return currgen.splice(0, 1)[0];
-  }
-}
-
-function cssify(root){
-  var elements = [];
-  var iter = iterator(root);
-  var el;
-  while((el = iter()) !== null){
-    var keys = Object.keys(el.styles);
-    var styles = [];
-    for(var i = 0 ; i < keys.length; i++){
-      styles.push(format(keys[i]) + ":" + format(el.styles[keys[i]]));
-    }
-    elements.push(styles);
-  }
-
-  var a = cc.compile(elements);
-
-  var classes = a['elements'];
-  var iter = iterator(root);
-  for(var i = 0; i < classes.length; i++){
-    if(classes[i].length !== 0){
-      iter().classes = classes[i].join(' ');
-    }
-  }
-
-  return createStyleSheet(a['styles']); 
-
-}
+var cs = require('./cssify.js');
 
 function createHTML(element){
   var open = "<" + element.tag;
@@ -96,7 +27,7 @@ function createHTML(element){
 }
 
 function generate(root){
-  var ssheet = cssify(root);
+  var ssheet = cs.cssify(root);
   var html = createHTML(root);
 
   return {'html' : html, 'css' : ssheet};
