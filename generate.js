47a1ecc7322517ba704e61d06a10be90a1537c67
Michael Jennings <mpjngs@gmail.com>
2015-04-04 16:31:41 -0700

    added deep variable sharing

--- generate.js
+++ generate.js
@@ -1,11 +1,13 @@
 
+module.exports = generate;
+
+var shareVars = require('./share.js');
 var cssify = require('./cssify.js');
-var selectorName = require('./csscompile.js').selectorName;
 var applyChildStyles = require('./styles.js').applyChildStyles;
 
 function generate(shared, legible){
 
-  var sharedScript = shareVars(this, shared);
+  var sharedScript = shareVars(shared, this.instance);
 
   var iter = this.iterator();
   var i;
@@ -18,44 +20,9 @@ function generate(shared, legible){
   return {'html' : html, 'css' : ssheet, 'js' : sharedScript};
 }
 
-module.exports = generate;
+
 
 //////////////////////////////////////////////////////////
-
-function shareVars(element, shared){
-  var sharedVars = Object.keys(shared);
-  var sharedScript = 
-        'var pbr = function(){' +
-           'function get(id){' +
-              'return function(){' + 
-                 'return document.getElementById(id)' + 
-              '}' + 
-            '}\n' +
-            'return {';
-  var newids = 0;
-  for(var i = 0; i < sharedVars.length; i++){
-    if(element.instance(shared[sharedVars[i]])){
-      var id;
-      var el = shared[sharedVars[i]];
-      if(el.attributes.id !== undefined){
-        id = el.attributes.id;
-      } else {
-        id = selectorName(newids);
-        newids++;
-        el.attribute('id', id);
-      }
-      sharedScript += "'" + sharedVars[i] + "': get('" + id + "'),";
-    } else {
-      sharedScript += "'" + sharedVars[i] + "':" + JSON.stringify(shared[sharedVars[i]]) + ",";
-    }
-  }
-  if(i > 0){
-    sharedScript = sharedScript.substring(0, sharedScript.length - 1) + '}}();';
-  } else {
-    sharedScript = '';
-  }
-  return sharedScript;
-}
 
 function toHTML(element, spaces){
   var indent = (spaces === undefined ? '' : spaces);
---
+++ share.js
@@ -0,0 +1,69 @@
+
+module.exports = shareVars;
+
+var selectorName = require('./csscompile.js').selectorName;
+
+//test is a function that tests whether an object is one of our Element objects
+function shareVars(shared, test){
+  var sharedScript = 
+        'var pbr = function(){' +
+           'function get(id){' +
+              'return function(){' + 
+                 'return document.getElementById(id)' + 
+              '}' + 
+            '}\n' +
+            'return ';
+
+  sharedScript += substitutedString(shared, test);
+  sharedScript += ' }();';
+
+  return sharedScript;
+}
+
+//test is a function that tests whether an object is one of our Element objects
+var newids = 0;
+function substitutedString(shared, test){
+  var ret = '';
+  if(test(shared)){
+    var id;
+    if(shared.attributes.id !== undefined){
+      id = shared.attributes.id;
+    } else {
+      id = selectorName(newids);
+      newids++;
+      shared.attribute('id', id);
+    }
+    ret = "get('" + id + "')";
+
+  } else if(Array.isArray(shared)){
+    ret = '[';
+    for(var i = 0; i < shared.length; i++){
+      ret += substitutedString(shared[i], test) + ',';
+    }
+    if(i > 0){//remove the trailing comma
+      ret = ret.substring(0, ret.length - 1);
+    }
+    ret += ']';
+
+  } else if(shared !== null & typeof shared === 'object'){
+    ret = '{';
+    var keys = Object.keys(shared);
+    for(var i = 0; i < keys.length; i++){
+      ret += keys[i] + ':';
+      ret += substitutedString(shared[keys[i]], test) + ',';
+    }
+    if(i > 0){//remove the trailing comma
+      ret = ret.substring(0, ret.length - 1);
+    }
+    ret += '}';
+
+  } else if(shared === null){
+    ret = 'null';
+
+  } else if(typeof shared === 'string') {
+    ret = "'" + shared + "'";
+  } else {
+    ret = shared;
+  }
+  return ret;
+}
