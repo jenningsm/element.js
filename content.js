40b7e8da0d62e92bcbf6eded49c26a99f81fe5f4
Michael Jennings <mpjngs@gmail.com>
2015-04-04 21:35:16 -0700

    changed content to allow objects of elements as parameter

--- content.js
+++ content.js
@@ -3,16 +3,21 @@
 function content(){
 
   for(var i = 0; i < arguments.length; i++){
-    if(Array.isArray(arguments[i])){
+    if((typeof arguments[i] === 'string') || this.instance(arguments[i])){
+      this.contentList.push(arguments[i]);
+    } else if(Array.isArray(arguments[i])){
       this.contentList = this.contentList.concat(arguments[i]);
     } else if(typeof arguments[i] === 'function'){
       for(var j = 0, item; (item = arguments[i](j)) !== null; j++){
         this.contentList.push(item);
       }
-    } else if((typeof arguments[i] === 'string') || this.instance(arguments[i])){
-      this.contentList.push(arguments[i]);
+    } else if(arguments[i] !== null && typeof arguments[i] === 'object'){
+      var keys = Object.keys(arguments[i]);
+      for(var j = 0; j < keys.length; j++){
+        this.contentList.push(arguments[i][keys[j]]);
+      }
     } else {
-      console.error("Unsupported contentList:", arguments[i]);
+      console.error("cannot insert ", arguments[i]);
     }
   }
 
